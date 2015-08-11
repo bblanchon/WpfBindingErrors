18f59f4da7da330f27c0593600b9d3ef06c50eda
Benoit Blanchon <github@benoitblanchon.fr>
2015-08-11 16:06:19 +0200

    Set version to 1.0.0

--- WpfBindingErrors/Properties/AssemblyInfo.cs
+++ WpfBindingErrors/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Benoit Blanchon")]
 [assembly: AssemblyProduct("WpfBindingErrors")]
-[assembly: AssemblyCopyright("Copyright © Benoit Blanchon 2013")]
+[assembly: AssemblyCopyright("Copyright © Benoit Blanchon 2013-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,4 +49,4 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyVersion("1.0.0")]
